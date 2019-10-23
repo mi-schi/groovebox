083a8a4b297b0f9832c986d30d55dcf942d6ad1e
michael.schindler <9NWbLxBAU-m4>
2019-10-23 10:49:38 +0200

    add delay to modifiers and disable recorders because of performance issue

--- modules/helper/modifier.py
+++ modules/helper/modifier.py
@@ -2,20 +2,25 @@ from pydub.effects import *
 from interfaces.potentiometer import *
 from typing import List
 from pydub import AudioSegment
+import time
 
 
-def add_volumes(volumes_raw: List[int], filtered_tones: List[AudioSegment], tones: List[AudioSegment]):
+def add_volumes(bpm, tact_number, volumes_raw: List[int], filtered_tones: List[AudioSegment], tones: List[AudioSegment]):
     old_volumes_raw = volumes_raw[:]
     while True:
+        sleep_time = 60 / bpm / tact_number
+        time.sleep(sleep_time)
         for i, volume_raw in enumerate(volumes_raw):
             if volume_raw != old_volumes_raw[i]:
                 filtered_tones[i] = tones[i] + calculate_volume(volume_raw)
                 old_volumes_raw[i] = volume_raw
 
 
-def add_filter(filters_raw: List[int], filtered_tones: List[AudioSegment]):
+def add_filter(bpm, tact_number, filters_raw: List[int], filtered_tones: List[AudioSegment]):
     old_filters_raw = filters_raw[:]
     while True:
+        sleep_time = 60 / bpm / tact_number
+        time.sleep(sleep_time)
         for i, filter_raw in enumerate(filters_raw):
             if filter_raw != old_filters_raw[i]:
                 if is_high_pass_filter(filter_raw):
